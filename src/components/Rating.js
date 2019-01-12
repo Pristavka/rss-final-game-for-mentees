f618a282566542e809543a53c335109df7010ed5
Anatolii Dzhudzhiev <dzhudzhiev@gmail.com>
2019-01-12 07:43:36 +0300

    feat: show retry button if heroku is down

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -3,32 +3,56 @@ import React, { Component } from 'react';
 class Rating extends Component {
   state = {
     users: [],
+    serverIsDown: false,
+    loading: true,
   };
 
+
   componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = () => {
     fetch('https://rs-game.herokuapp.com/results')
       .then(res => res.json())
-      .then(data => this.setState({ users: data }))
-      .catch(console.log);
+      .then(data => this.setState(
+        {
+          users: data,
+          serverIsDown: false,
+          loading: false,
+        },
+      ))
+      .catch(() => this.setState({ serverIsDown: true }));
   }
 
   render() {
     const { backToOptions } = this.props;
-    const { users } = this.state;
+    const { users, serverIsDown, loading } = this.state;
+
+    if (loading) {
+      return (
+        <div>
+          <h2>Rating</h2>
+          <p>Loading...</p>
+        </div>
+      );
+    }
 
     const usersTable = users.map(user => (
       <p key={user._id}>
         <span>{user.name}</span>
          :
-         <span>{user.monstersCount}</span>
+        <span>{user.monstersCount}</span>
       </p>
     ));
+
+    const retryButton = <button type="submit" onClick={this.fetchData}>Retry</button>
 
     return (
       <div>
         <div className="rating">
           <h2>Rating</h2>
-          {usersTable}
+          {serverIsDown ? retryButton : usersTable}
         </div>
         <button type="submit" onClick={backToOptions}>Back</button>
         <style jsx>
