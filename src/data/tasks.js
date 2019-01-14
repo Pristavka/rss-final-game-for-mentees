2312d968eb9d832b066d770f6610358abfb653e1
jamadamur <ancuent@gmail.com>
2019-01-14 20:01:05 +0300

    feat: questions now generates randomly

--- src/data/tasks.js
+++ src/data/tasks.js
@@ -13,94 +13,357 @@ export default {
   "theme": "Лишнее слово"
 },
 {
-  "q": "Измени одну букву в слове и запиши новое слово - из <b>золота</> можно получить гнилое место",
-  "a": "болото",
-  "level": "легко",
-  "theme": "Измени букву"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "самолёт", "correct": false},
+ {"option": "пароход", "correct": true},
+ {"option": "воздушный шар", "correct": false},
+{"option": "вертолёт", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "Напиши правильно измененное слово в крылатом выражении. Париж стоит мацы",
-  "a": "мессы",
-  "level": "сложно",
-  "theme": "Крылатые выражения"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "автомобиль", "correct": false},
+ {"option": "фургон", "correct": false},
+ {"option": "самолёт", "correct": true},
+{"option": "грузовик", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "Как звали кота, который хотел подружиться с мышами?",
-  "a": "Леопольд",
-  "level": "сложно",
-  "theme": "Сказочные герои"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "диван", "correct": false},
+ {"option": "шкаф", "correct": false},
+ {"option": "лампа", "correct": true},
+{"option": "стол", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "Выбери правильный вариант. Скачет наш котёнок ловко, очень любит он...",
-  "a": [
-	{"option": "молоко", "correct": true},
-	{"option": "морковку", "correct": false}
-  ],
-  "select_any": true,
-  "level": "легко",
-  "theme": "загадки"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "волк", "correct": false},
+ {"option": "лиса", "correct": false},
+ {"option": "кот", "correct": true},
+{"option": "медведь", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "У Монстрика 7 разных пар носков. Все носки перепутались. Монстрик вытягивает носки не глядя, и ему совсем не везет. Какое наименьшее количество носков придется достать, чтобы наверняка получить пару одинаковых носков?",
-  "a": [
-	{"option": "8", "correct": true},
-	{"option": "7", "correct": false},
-	{"option": "14", "correct": false},
-	{"option": "6", "correct": false}
-  ],
-  "select_any": true,
-  "correct": "<p>Правильно!</p>",
-  "incorrect": "<p>Подумай еще</p>",
-  "level": "легко",
-  "theme": "Комбинаторика"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "молоко", "correct": false},
+ {"option": "творог", "correct": true},
+ {"option": "сметана", "correct": false},
+{"option": "хлеб", "correct": true}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "Как переводится с английского “morning”? Напиши ответ",
-  "a": "утро",
-  "level": "легко",
-  "theme": "Английский язык",
-  "correct": "<p>Правильно!</p>",
-  "incorrect": "<p>Подумай еще</p>"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "храбрый", "correct": false},
+ {"option": "злой", "correct": true},
+ {"option": "смелый", "correct": false},
+{"option": "отважный", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "Выбери, правда или  ложь. Банка обычной колы тонет в воде, а банка диетической — всплывает",
-  "a": [
-	{"option": "Правда", "correct": true},
-	{"option": "Ложь", "correct": false}
-  ],
-  "select_any": true,
-  "correct": "<p>Правильно!</p>",
-  "incorrect": "<p>Подумай еще</p>",
-  "level": "легко",
-  "theme": "Правда или ложь"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "блокнот", "correct": true},
+ {"option": "телевизор", "correct": false},
+ {"option": "радио", "correct": false},
+{"option": "ноутбук", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "Определи, правда ли два выражения - до союза И(ИЛИ) и после, и дай общий ответ. Страус ИЛИ слон – птица",
-  "a": [
-	{"option": "Правда", "correct": true},
-	{"option": "Ложь", "correct": false}
-  ],
-  "select_any": true,
-  "correct": "<p>Правильно!</p>",
-  "incorrect": "<p>Подумай еще</p>",
-  "level": "легко",
-  "theme": "Логика"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "ложка", "correct": false},
+ {"option": "тарелка", "correct": false},
+ {"option": "кастрюля", "correct": false},
+{"option": "сумка", "correct": true}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "Посчитай, сколько будет 39 / 3 + 3 = ? ",
-  "a": "16",
-  "correct": "<p>Правильно!</p>",
-  "incorrect": "<p>Подумай еще</p>",
-  "level": "легко",
-  "theme": "Математика"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "старый", "correct": false},
+ {"option": "дряхлый", "correct": false},
+ {"option": "маленький", "correct": true},
+{"option": "ветхий", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
 },
 {
-  "q": "Батон разрезали на три части. Сколько сделали разрезов? (введи число)",
-  "a": "2",
-  "correct": "<p>Правильно!</p>",
-  "incorrect": "<p>Подумай еще</p>",
-  "level": "легко",
-  "theme": "Занимательные задачки"
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "платье", "correct": false},
+ {"option": "свитер", "correct": false},
+ {"option": "шапка", "correct": true},
+{"option": "рубашка", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "мыло", "correct": true},
+ {"option": "метла", "correct": false},
+ {"option": "зубная паста", "correct": false},
+{"option": "шампунь", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "час", "correct": false},
+ {"option": "минута", "correct": false},
+ {"option": "лето", "correct": true},
+{"option": "секунда", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "ветер", "correct": false},
+ {"option": "дождь", "correct": false},
+ {"option": "весна", "correct": true},
+{"option": "снег", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "сосна", "correct": false},
+ {"option": "гвоздика", "correct": true},
+ {"option": "береза", "correct": false},
+{"option": "дуб", "correct": false}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "длинный", "correct": false},
+ {"option": "короткий", "correct": false},
+ {"option": "высокий", "correct": false},
+{"option": "новый", "correct": true}
+],
+"select_any": true,
+"level": "легко",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "самолёт", "correct": false},
+ {"option": "поезд", "correct": true},
+ {"option": "вертолёт", "correct": false},
+ {"option": "ворона", "correct": false},
+{"option": "шмель", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "километр", "correct": false},
+ {"option": "миля", "correct": false},
+ {"option": "миллиметр", "correct": false},
+ {"option": "дюйм", "correct": false},
+{"option": "килограмм", "correct": true}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "арбуз", "correct": false},
+ {"option": "земляника", "correct": false},
+ {"option": "слива", "correct": true},
+{"option": "черника", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "лето", "correct": false},
+ {"option": "утро", "correct": true},
+ {"option": "зима", "correct": false},
+{"option": "весна", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "клоун", "correct": false},
+ {"option": "фокусник", "correct": false},
+ {"option": "кассир", "correct": true},
+{"option": "дрессировщик", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "бабушка", "correct": false},
+ {"option": "сын", "correct": false},
+ {"option": "папа", "correct": false},
+{"option": "мальчик", "correct": true}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "инженер", "correct": false},
+ {"option": "парикмехер", "correct": false},
+ {"option": "пловец", "correct": true},
+{"option": "воспитатель", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "хлопок", "correct": false},
+ {"option": "шерсть", "correct": false},
+ {"option": "акрил", "correct": true},
+ {"option": "шёлк", "correct": false},
+{"option": "лён", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "смотреть", "correct": false},
+ {"option": "глядеть", "correct": false},
+ {"option": "моргать", "correct": true},
+{"option": "наблюдать", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "дождь", "correct": false},
+ {"option": "снег", "correct": false},
+ {"option": "град", "correct": false},
+ {"option": "молния", "correct": true},
+{"option": "изморось", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "мерин", "correct": true},
+ {"option": "волчица", "correct": false},
+ {"option": "лань", "correct": false},
+ {"option": "кобыла", "correct": false},
+{"option": "овца", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "превосходный", "correct": false},
+ {"option": "поэтичный", "correct": true},
+ {"option": "величавый", "correct": false},
+ {"option": "грандиозный", "correct": false},
+{"option": "величественный", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "ель", "correct": false},
+ {"option": "сосна", "correct": false},
+ {"option": "липа", "correct": true},
+ {"option": "кедр", "correct": false}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+},
+{
+"q": "Выбери лишнее слово:",
+"a": [
+  {"option": "Москва", "correct": false},
+ {"option": "Лондон", "correct": false},
+ {"option": "Париж", "correct": false},
+{"option": "Нью-Йорк", "correct": true}
+],
+"select_any": true,
+"level": "сложно",
+"theme": "Лишнее слово"
+}]
 }
-]}
-
